7d8e12b5ac78cabb141edd54790bd9ec21956cb3
joshmarinacci <joshua@marinacci.org>
2026-01-05 08:41:27 -0800

    improve box styling with unicode box symbols

--- src/debugger2/util.ts
+++ src/debugger2/util.ts
@@ -1,6 +1,10 @@
 import process from "node:process";
 import type {ViewOutput} from "./model.ts";
 
+const GREEN = '\x1b[32m';
+const GREEN_BOLD = '\x1b[1;32m'
+const RESET = '\x1b[0m'; // Resets all attributes
+
 export function clear_screen() {
     process.stdout.write("\x1Bc");
 }
@@ -12,6 +16,43 @@ export function Header(stack: string, bold: boolean) {
         `${ch.repeat(5)} ${stack} ${ch.repeat(5)}`
     ];
 }
+
+type Border = {
+    TopLeft:string,
+    Top:string,
+    TopRight:string,
+    Left:string,
+    Center:string,
+    Right:string,
+    BottomLeft:string,
+    Bottom:string,
+    BottomRight:string,
+}
+type Style = {
+    attribute:string,
+    border: Border
+}
+
+function stringToBorder(str:string):Border {
+    return {
+        TopLeft: str[0],
+        Top: str[1],
+        TopRight: str[2],
+        Left: str[3],
+        Center: str[4],
+        Right: str[5],
+        BottomLeft: str[6],
+        Bottom: str[7],
+        BottomRight: str[8]
+    }
+}
+
+const STYLE:Style = {
+    attribute: '\x1b[31m',
+    border: stringToBorder('+-+| |+-+')
+}
+
+STYLE.border = stringToBorder('┌─┐│ │└─┘')
 
 export class BoxFrame {
     private width: number;
@@ -36,25 +77,31 @@ export class BoxFrame {
 
     render(): ViewOutput {
         let output = []
-        let ch = '-'
+        let style = STYLE
         if (this.active) {
-            ch = '='
+            style = {
+                attribute:GREEN_BOLD,
+                border:STYLE.border
+            }
         }
-        let margin = Math.floor((this.width - this.name.length - 2) / 2)
-        let header = '+' + ch.repeat(margin) + ' ' + this.name + ' ' + ch.repeat(margin) + '+'
-        output.push(header)
-        let border = '|'
-        if (this.active) {
-            border = '!'
-        }
-        this.content.forEach((str, n) => {
-            let len = str.length
-            let pad = Math.max(this.width - str.length - 3,0)
 
-            output.push(`${border} ${str} ${' '.repeat(pad)}${border}`)
+        let margin = Math.floor((this.width - this.name.length - 3) / 2)
+        let right_pad = 0
+        if(this.name.length % 2 == 0) {
+            right_pad +=1
+        }
+        let header = style.border.TopLeft
+            + style.border.Top.repeat(margin)
+            + ' ' + this.name + ' '
+            +style.border.Top.repeat(margin+right_pad)
+            + style.border.TopRight
+        output.push(`${style.attribute}${header}${RESET}`)
+        this.content.forEach((str, n) => {
+            let pad = Math.max(this.width - str.length - 3,0)
+            output.push(`${style.attribute}${style.border.Left} ${str} ${' '.repeat(pad)}${style.border.Right}${RESET}`)
         })
-        let footer = '+' + ch.repeat(this.width - 1) + '+'
-        output.push(footer)
+        let footer = `${style.border.BottomLeft}${style.border.Bottom.repeat(this.width-1)}${style.border.BottomRight}`
+        output.push(`${style.attribute}${footer}${RESET}`)
         return output
     }
 }
